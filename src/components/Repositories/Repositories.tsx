b5af046f9da99e1b96250b63f0308b69ef918c19
georgesamir21 <georges.samir21@gmail.com>
2020-09-04 19:34:15 +0200

    add users repos  components

---
+++ src/components/Repositories/Repositories.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Repositories extends Component {
+  render() {
+    return (
+      <div>
+        <p>Reposatories Component Works!</p>
+      </div>
+    );
+  }
+}
+
+export default Repositories;
---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <p>Users Component Works!</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
